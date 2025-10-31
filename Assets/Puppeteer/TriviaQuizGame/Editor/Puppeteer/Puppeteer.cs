d0f87b92d67b93505e6f05488318d8413f847128
Renke Huang <zhuyaosheng51122@gmail.com>
2025-10-31 02:48:53 +0000

    initial commit

---
+++ Assets/Puppeteer/TriviaQuizGame/Editor/Puppeteer/Puppeteer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Puppeteer
+{
+    [InitializeOnLoad]
+    public class Puppeteer : EditorWindow
+    {
+        static Puppeteer()
+        {
+            if (PlayerPrefs.GetInt("Puppeteer", 0) == 0) EditorApplication.update += Startup;
+        }
+
+        static void Startup()
+        {
+            EditorApplication.update -= Startup;
+
+            EditorApplication.ExecuteMenuItem("Tools/More From Puppeteer");
+
+            PlayerPrefs.SetInt("Puppeteer", 1);
+        }
+    }
+}
